3de292934659f3fd52eeee091be05022fe8acc4e
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2026-03-30 18:08:55 +0700

    added colour

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white shadow-xl rounded-2xl p-10 text-center">
-        <h1 className="text-3xl font-bold text-blue-600 mb-4">
-          Hello dari React + Inertia 🚀
-        </h1>
-        <p className="text-gray-600">
-          Tailwind sudah berhasil dipakai 🎉
-        </p>
-        <button className="mt-6 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
-          Klik Saya
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-tertiary flex items-center justify-center">
+            <div className="bg-white p-10 rounded-xl shadow text-center">
+                <h1 className="text-primary text-3xl font-bold">
+                    Mainan Kayu Anak
+                </h1>
+
+                <button className="mt-6 bg-secondary text-white px-6 py-2 rounded-lg hover:opacity-90">
+                    Beli Sekarang
+                </button>
+            </div>
+        </div>
+    );
 }
